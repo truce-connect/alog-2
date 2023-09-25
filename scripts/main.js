9aef1e659f368f83a0cb2662a0b00d2c0ad7d2bb
truce-connect <tayotruce@gmail.com>
2023-09-25 23:39:26 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,212 @@
+
+// // // // console.log("hello world")
+// // // // console.log("truce")
+// // // // document.write("truce-up")
+
+// // // // var urname= prompt("what your name")
+// // // // document.write(urname)
+// // // //     // step 1 we store 1 variable;
+// // // //     // step 2 we store  2 container 
+// // // //     //  step 3 we create  an empty cointianer;
+// // // //     // step 4 re-store the first container  into the third one 
+// // // //     // step 5 re-store the secon container into first
+// // // //     //  step 6 resore third container into second 
+
+// // // // // step 1 we store 1 variable;
+// // // //  let con1= "purity";
+// // // //  console.log(con1)
+// // // //  // step 2 we store  2 container 
+// // // //  let con2 ="samiat";
+// // // //  console.log(con2)
+// // // // //  step 3 we create  an empty cointianer; 
+// // // //  let con3  ;
+// // // //  // step 4 re-store the first container  into the third one 
+// // // // con3 = con1
+// // // // // step 5 re-store the secon container into first
+// // // //   con1 =con2
+// // // //   //  step 6 resore third container into second
+// // // //   con2=con3
+// // // //  console.log(con1)
+// // // //  console.log(con2)
+
+// // // // // step 1 create a variable  that will store  the username
+// // // // const username = prompt("username");
+// // // // // step 2   create variable that store  the greateting 
+// // // //  const greetings = prompt(" enter greeting ")
+// // // // // step 3  create a variable that return  both variable  as outpput
+// // // //  const output = alert(greetings + " " + username) 
+
+// // // //  step 1 create a variable that will store the  first no?
+// // // const firstnum  = prompt("enter first num") ;
+// // // //  step2  create variable for the second no
+// // // const  secondnum = prompt("enter secondnum");
+
+// // // //  step 3 convert and add  console .log out put
+// // // console.log(Number(firstnum) + Number(secondnum))
+
+// // //  step 1 create fubction 
+// // //  step 2 create a variable that store the name 
+// // //  step 3 create a variable that store the city
+// // //  step 4 create  AN alert that return both 
+// // //  call the function
+
+// // // function greet(){
+// // //   const ask1 =prompt("what is your name ")
+// // //    const ask2 = prompt("where you from")
+
+// // // alert("hello" + " " + ask1 + " " + "from" + " "+ ask2 + " ")
+// // // }
+// // //  greet()
+// // //  greet()
+// // //  greet()
+// // //  greet()
+// // //  greet()
+
+// // // step1 : create a variable  that store their name  variable using prompt
+ 
+// // //  step 2: create a variable that store the coures 1 course  using prompt 
+
+// // //  step 3: create a variable that store the coures 2 course  using prompt
+
+// // //  step 4: create a variable that store the coures 3 course  using prompt
+
+// // //  step 5: create a variable that store the coures 4 course  using prompt
+
+// // //  step 6: create a variable that store the coures 5 course  using prompt 
+// // //  step 7 : then we  return  the output in the  connsole  to show the  result 
+
+// // function enteri() {
+// //   const nami = prompt("enter your full name ")
+// //   console.log( "Name:"+ nami)
+// //   const course1  = prompt("enter the title of course 1")
+// //   console.log( "course 1:"+course1)
+// //   const course2  = prompt("enter the title of course 2")
+// //   console.log("course 2 :"  + course2)
+// //   const course3  = prompt("enter the title of course 3") 
+// //   console.log("course 3:"+ course3 )
+// //   const course4 = prompt("enter the title of course 4")
+// //   console.log( "course 4:" + course4 )
+// //   const course5 = prompt("enter the title of course 5")
+// //   console.log( "course 4:" + course5)   
+// // }
+// //  enteri()
+   
+// // greet-User fnunction
+
+ 
+// // //    step 1 create a function that have a greet user as the name 
+// //  function greetuser(){
+// //  }
+// //   //  step 2 put an argument as the parameter inside the function to store the name 
+// //   function greetuser(cname){
+// //   }
+// //   //  step  3 create a console.log with in a string in it has good morning and concat the name 
+// //   function greetuser(cname){
+// //     console.log("Good-day"+ " " +cname)
+// //    }
+// //   //  step 4 return the function
+// //  greetuser("tolu")
+// //  greetuser("tope")
+// //  greetuser("micheal")
+// // step1 create the fuction 
+// //  step2  create container as parameter
+// //  step3  return it in the console  by converting it to a number and adding
+// // step4  decarle the function
+// //   function add2num(firstnum,secondnum) {
+    
+// //     console.log(Number(firstnum)+Number(secondnum))
+    
+// //   }
+// // add2num(3,4)
+
+// // function multiply(round1,round2) {
+// //   console.log(Number(round1)*Number(round2))
+  
+// // }
+
+
+// // multiply(8,5)
+// //  step 1create a function with the name square function
+// //  give it an arguement that store it as variable 
+// //  covert it  to a number 
+// //  return as a console 
+// // function square(no){
+
+// //   console.log(Number(no**2))
+// // }
+// //  square(6)
+
+// // function square(no){
+
+// //   console.log(Number(no**3))
+// // }
+// //  square(6)n
+// //  function concator(str1,str2) {
+// //   console.log(str1 +" "+ str2)
+  
+// //  }
+// // concator("love is", "blind")
+// //  function level(stro) {
+// //   return stro.length
+  
+// //  }
+// //  level(GOD)
+
+// function countchar() {
+    
+//     let sentence = prompt()
+// }
+
+
+// // const hour = 6;
+// // if (hour >=6 && hour <12)
+// //     console.log("good-morning")
+
+// // else if (hour >=12 && hour > 6) 
+// //     console.log("good-afternoon")
+// // else console.log("good evening")
+ 
+// // function gradecheck(){
+// //     const ask =prompt("what is your grade")
+// //       if (ask >=90 && ask <=100){
+// //           console.log("you got an A: EXCELLENT ")
+// //       }
+// //           else if (ask >=75 && ask  <=89){
+  
+// //                console.log("you got a B: VERY GOOD ")
+// //               }
+// //               else if (ask >=65 && ask<=74 ){
+  
+// //                console.log("you got a C : GOOD")
+// //               }
+// //                else if (ask >=55 &&  ask >=64){
+  
+// //               console.log("you a D:fair")
+// //               }
+
+// //             }
+//    function 
+
+function guestcheck() {
+    const guess = ["purity","diana","josh","wizkid"]
+    const acceptguest = prompt("enter your name ").toLowerCase() 
+      for (let i = 0; i<guess.length; i++) {
+        if( acceptguest === guess[i]){
+            console.log("you welcome to my party ")
+        }else{
+          console.log("get out we  no sabi your papa")
+        }
+       
+      }
+       
+    }
+guestcheck()
+ const markweight= 78;
+  const markheight = 1.69;
+ const johnweight =92;
+ const johnheight = 1.95;
+  const bmi = markweight/(markheight**2)
+  const  bmi2 = johnweight/(johnheight **2)
+   console.log(bmi)
+    console.log(bmi2)
+     console.log("mark bmi is"+" "+bmi+"john bmi is" +bmi2+"john bmi is greater than mark " )
